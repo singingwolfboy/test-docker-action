f0560d65b7e251bbb379026edbd05b6581973bfd
David Baumgold <david@davidbaumgold.com>
2020-05-04 16:37:32 +0200

    double dash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 touch abc.deb abc-dbgsym.deb
-filename=`ls *.deb | grep -v -dbgsym`
-dbgsym=`ls *.deb | grep -dbgsym`
+filename=`ls *.deb | grep -v -- -dbgsym`
+dbgsym=`ls *.deb | grep -- -dbgsym`
 echo ::set-output name=filename::$filename
 echo ::set-output name=filename-dbgsym::$dbgsym
